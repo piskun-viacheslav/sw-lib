b42481e174848b83cc7d0acdaab101a91d728d4a
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-11-04 10:58:07 +0200

    created Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './header.scss';
+
+const Header = () => (
+    <header className="header">
+        <div className="header__logo"></div>
+        <nav className="header__nav nav">
+            <ul className="nav__list">
+                <li className="nav__item"><NavLink className="nav__link" to="/people">People</NavLink></li>
+                <li className="nav__item"><NavLink className="nav__link" to="/planets">Planets</NavLink></li>
+                <li className="nav__item"><NavLink className="nav__link" to="/starships">Starships</NavLink></li>
+            </ul>
+        </nav>
+    </header>
+);
+
+export default Header;
