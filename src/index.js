399797cf5306255a57e28159cdc90bf6f3ce214f
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-11-04 11:28:04 +0200

    added routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
 import App from './components/App';
 import './index.scss';
 
 ReactDOM.render(
-    <App />,
+    <HashRouter>
+        <App />
+    </HashRouter>,
     document.getElementById('root')
 );
