6c3999cb74c22e99435c597d4fb39be8927cada8
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-11-06 00:29:29 +0200

    added logic and routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { Route } from "react-router";
+import SWService from "../../utils/swService";
+
+import Header from "../Header";
+import SearchBar from "../SearchBar";
+import LibraryList from "../LibraryList";
+import LibraryItem from "../LibraryItem";
+import Person from "../Person";
+import Planet from "../Planet";
+import Starship from "../Starship";
+
 import './app.scss';
 
-const App = () => (
-    <div>Hello SW-LIB</div>
-);
+const sw = new SWService();
+const App  = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <SearchBar/>
+            <Route path={'/people'} exact
+                render={
+                    ({match}) => <LibraryList
+                        getData={sw.getAllPeople}
+                        match={match}
+                    />
+                }/>
+            <Route path={'/planets'} exact
+               render={
+                   ({match}) => <LibraryList
+                       getData={sw.getAllPlanets}
+                       match={match}
+                   />
+               }/>
+            <Route path={'/starships'} exact
+                render={
+                    ({match}) => <LibraryList
+                       getData={sw.getAllStarships}
+                       match={match}
+                   />
+                }/>
+            <Route path={'/people/:id'} render={ ({match}) => (
+                <LibraryItem
+                   id={match.params.id}
+                   getData={sw.getPerson}
+                   viewData={(data) => <Person person={data} />}
+                />)
+            }/>
+           <Route path={'/planets/:id'} render={ ({match}) => ( <LibraryItem
+                   id={match.params.id}
+                   getData={sw.getPlanet}
+                   viewData={(data) => <Planet planet={data} />}
+               />)
+           }/>
+
+            <Route path={'/starships/:id'} render={ ({match}) => ( <LibraryItem
+                    id={match.params.id}
+                    getData={sw.getStarship}
+                    viewData={(data) => <Starship starship={data} />}
+                />)
+            }/>
+        </div>
+    )
+};
 
 export default App;
