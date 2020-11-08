6241100cf8b0a7b07c0493c7909e964e2dbef832
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-11-08 21:58:55 +0200

    add new NavLink for Home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => (
         <div className="header__logo"></div>
         <nav className="header__nav nav">
             <ul className="nav__list">
+                <li className="nav__item"><NavLink className="nav__link" to="/" exact>Home</NavLink></li>
                 <li className="nav__item"><NavLink className="nav__link" to="/people">People</NavLink></li>
                 <li className="nav__item"><NavLink className="nav__link" to="/planets">Planets</NavLink></li>
                 <li className="nav__item"><NavLink className="nav__link" to="/starships">Starships</NavLink></li>
