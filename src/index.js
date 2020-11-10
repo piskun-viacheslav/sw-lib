26299dfd8b35fb9d47045f41a380a76f436356f6
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-11-10 21:52:07 +0200

    config project to deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './components/App';
 import './index.scss';
 
 ReactDOM.render(
-    <HashRouter>
+    <HashRouter basename='/'>
         <App />
     </HashRouter>,
     document.getElementById('root')
