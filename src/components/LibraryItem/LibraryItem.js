67c5698e742845120cffd8542b9914c68acde032
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-11-12 23:03:54 +0200

    use ImageBlock component in LibraryItem

--- src/components/LibraryItem/LibraryItem.js
+++ src/components/LibraryItem/LibraryItem.js
@@ -2,20 +2,18 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import FieldList from "../FieldList";
+import ImageBlock from "../ImageBlock";
 
 import styles from './index.module.scss';
 
 const LibraryItem = ({ data, classList = ''}) => {
-    const { info, name, imageUrl } = data;
+    const { info, name, imageUrl, defaultImageUrl } = data;
 
     return (
         <div className={classNames(styles.item, classList)}>
             <h2 className={styles.title}>{ name }</h2>
             <div className={styles.imageContainer}>
-                <img className={styles.image}
-                     src={imageUrl}
-                     alt={name}
-                />
+                <ImageBlock url={imageUrl} defaultUrl={defaultImageUrl}/>
             </div>
             <FieldList fieldList={ info } />
             <button className="backButton" type="button">&#60;&#60; Back</button>
