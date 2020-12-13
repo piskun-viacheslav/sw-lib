c80a9bf9925a4440d1ef8086a45dbeed361f3b3d
VyacheslavPiskun <piskun.vyacheslav@corp.kismia.com>
2020-12-13 11:41:28 +0200

    create SearchResults component

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+// import PropTypes from 'prop-types';
+import CardsList from "../CardsList";
+import styles from './index.module.scss'
+
+const SearchResults = ({foundItems, url, closeResults }) => (
+    <Fragment>
+        <button
+            type="button"
+            className={styles.closeResultsButton}
+            onClick={closeResults}
+        >+</button>
+        <CardsList data={foundItems} url={url} classNames={'LibraryList'}/>
+    </Fragment>
+);
+
+export default SearchResults;
